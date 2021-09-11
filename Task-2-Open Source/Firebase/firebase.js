4c636d4303b9579bf0d5e661e90cfcfaceb1087a
Aniket Mishra <82095877+AnIkeT126@users.noreply.github.com>
2021-09-11 18:59:52 +0530

    Add files via upload

---
+++ Task-2-Open Source/Firebase/firebase.js
@@ -0,0 +1,13 @@
+var firebaseConfig = {
+    
+apiKey: "AIzaSyBy7QyHQn71zXgJrXg-qbOxu_z4zQf37e0",
+authDomain: "basic-a47cb.firebaseapp.com",
+projectId: "basic-a47cb",
+storageBucket: "basic-a47cb.appspot.com",
+messagingSenderId: "971561084849",
+appId: "1:971561084849:web:4896b43224764456147f90",
+measurementId: "G-3RX47GFR79"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
